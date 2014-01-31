6dc413978b14b28430918bcc2256fd44763da3b6
Silas Sewell <silas@sewell.org>
2014-01-31 13:40:27 -0800

    Update task-cli

--- task-cli.rb
+++ task-cli.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class TaskCli < Formula
   homepage 'https://github.com/shutterstock/task'
-  url 'https://github.com/shutterstock/task/archive/a107481266cc567314377b248e145434f5b694bb/gh-pages.zip'
-  version '0.0.1-a107481266cc567314377b248e145434f5b694bb'
-  sha1 '51520165b12cdd6d9ee391740ba0e3311dde6450'
+  # https://github.com/shutterstock/task/commits/gh-pages/build/perl-5.8/task
+  url 'https://github.com/shutterstock/task/archive/5b54157e7e2d13bfe0baf350559f5c0bd8433064/gh-pages.zip'
+  version '0.0.2-5b54157e7e2d13bfe0baf350559f5c0bd8433064'
+  sha1 'bb3b1dce4d2e215d0a8365e2178b80424cdd4cc7'
 
   def install
     bin.mkpath
