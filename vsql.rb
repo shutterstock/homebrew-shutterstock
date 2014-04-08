ba62b686b6eeaafd4aaf6d2bead6bd7981e40fde
Silas Sewell <silas@sewell.org>
2014-04-08 15:07:08 -0700

    Fix sha1 for vsql, confirmed upstream

--- vsql.rb
+++ vsql.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Vsql < Formula
   homepage 'https://my.vertica.com'
   url 'http://dl.shuttercorp.net/homebrew/vertica-client-6.1.3-0.mac.tar.gz'
-  sha1 '32a06c0b38e531f464d6e3925f07cbc4944bf7b4'
+  sha1 'b1c758e51d167f17d50a10bac2273eeb056f56c3'
 
   def install
     bin.install 'vertica/bin/vsql'
